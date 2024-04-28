93163d9a266bc426bd6c5a4168bf7336c1677798
Ta Hong Phuc <taphuc1@gmail.com>
2024-04-29 00:03:54 +0700

    fix error

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -9,7 +9,7 @@ const EditTodo = ({todo}) => {
         e.preventDefault();
          try {
             const body = { description };
-            const respond = await fetch(`http://localhost:5005/todos/, {
+            const respond = await fetch(`http://localhost:5005/todos/${todo.todo_id}`, {
                 method:"PUT",
                 headers: {"Content-Type": "application/json" },
                 body: JSON.stringify(body)
